db1adadc12f62f845d4dfe150ee55ce2113608e7
jhilmiljain <41698967+jhilmiljain@users.noreply.github.com>
2019-09-03 14:22:43 +0530

    Update sample.py

--- rules/sample.py
+++ rules/sample.py
@@ -3,9 +3,6 @@ name: Sample
 description: "Rule which sends an email when an email from jhilmiljain@gmail.com is received"
 pack: email
 enabled: false
-trigger:
-  type: email.imap.message
-  parameters: {}
 criteria:
   trigger.from:
     pattern: jhilmiljain@gmail.com
